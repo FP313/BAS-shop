1551073384665fbf353105aad8f8b86e0494776c
FP313 <23po1_4@pkgt.local>
2026-06-24 10:46:13 +0500

    last13233211232

--- backend/public/js/intersecting.js
+++ backend/public/js/intersecting.js
@@ -3,9 +3,12 @@ import { getReviews } from "../js/generiticReviews.js"
 
 const moreCatalog = document.querySelector(".moreCatalog");
 const moreReviwes = document.querySelector(".reviewsTextmore");
+const moreCatalogOffer = document.querySelector(".similarProductsTextmore");
+
 
 moreCatalog.addEventListener("click", ()=>{viewMore(10)});
 moreReviwes.addEventListener("click", ()=>{viewMore(10)});
+moreCatalogOffer.addEventListener("click", ()=>{viewMore(10)});
 
 function viewMore(count){
     getCards(count);
